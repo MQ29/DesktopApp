e8ea5720cadf20122e1a59d518d5ddd27f626ddc
MQ29 <matiax2829@gmail.com>
2023-06-20 19:26:39 +0200

    GetByUsername fixed

---
+++ Logindb/ViewModel/MainViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using Logindb.Model;
+using Logindb.Repositories;
+
+namespace Logindb.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private UserAccountModel _currentUserAccount;
+        private IUserRepository _userRepository;
+
+        public UserAccountModel CurrentUserAccount
+        {
+            get { return _currentUserAccount; }
+            set
+            {
+                _currentUserAccount = value;
+                OnPropertyChanged(nameof(CurrentUserAccount));
+            }
+        }
+
+        public MainViewModel()
+        {
+            _userRepository = new UserRepository();
+            LoadCurrentUserData();
+        }
+
+        private void LoadCurrentUserData()
+        {
+            var user = _userRepository.GetByUsername(Thread.CurrentPrincipal.Identity.Name);
+            if (user != null)
+            {
+                CurrentUserAccount = new UserAccountModel()
+                {
+                    Username = user.Username,
+                    DisplayName = $"Welcome {user.Name} {user.LastName};)",
+                    ProfilePicture = null
+                };
+            }
+            else
+            {
+                MessageBox.Show("Invalid user, not logged in");
+                Application.Current.Shutdown();
+            }
+
+
+        }
+    }
+}
